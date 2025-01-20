e59e6b0dd202a925f3e452d7e1992e845268ee07
Joel Sikström <joel.sikstrom@oracle.com>
2025-01-20 10:37:55 +0100

    No NUMA support on Windows so just get first manager

--- src/hotspot/os/windows/gc/z/zVirtualMemory_windows.cpp
+++ src/hotspot/os/windows/gc/z/zVirtualMemory_windows.cpp
@@ -222,7 +222,7 @@ void ZVirtualMemoryManager::pd_initialize_before_reserve() {
 }
 
 void ZVirtualMemoryManager::pd_initialize_after_reserve() {
-  _impl->initialize_after_reserve(&_manager);
+  _impl->initialize_after_reserve(&_managers.get(0));
 }
 
 bool ZVirtualMemoryManager::pd_reserve(zaddress_unsafe addr, size_t size) {

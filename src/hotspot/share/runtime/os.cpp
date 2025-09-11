0e5b28007174c5652c5f7d21677cb4e8fa5998e3
Axel Boldt-Christmas <axel.boldt-christmas@oracle.com>
2025-09-11 06:42:44 +0000

    UPSTREAM: Add os::compressed_memory
    This reverts commit 9666853793980340db6b30373f03639b44a2163c.

--- src/hotspot/share/runtime/os.cpp
+++ src/hotspot/share/runtime/os.cpp
@@ -2226,6 +2226,14 @@ bool os::used_memory(size_t& value) {
   return true;
 }
 
+bool os::compressed_memory(size_t &value) {
+#ifdef __APPLE__
+  return os::Bsd::compressed_memory(value);
+#else
+  return false;
+#endif
+}
+
 bool os::commit_memory(char* addr, size_t bytes, bool executable) {
   assert_nonempty_range(addr, bytes);
   bool res = pd_commit_memory(addr, bytes, executable);
